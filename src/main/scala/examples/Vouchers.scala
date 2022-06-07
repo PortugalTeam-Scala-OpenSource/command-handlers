67e43d3d56e1747283c0b3543280610fc5d98577
Miguel Lemos <miguellemos95@gmail.com>
2022-06-07 17:52:04 +0200

    Create Vouchers.scala

---
+++ src/main/scala/examples/Vouchers.scala
@@ -0,0 +1,28 @@
+package examples
+
+object Vouchers extends App {
+ 
+  case class VoucherId(id: String)
+  sealed trait Commands 
+  case class AddVoucher(voucherId: VoucherId) extends Commands
+  case class RemoveVoucher(voucherId: VoucherId) extends Commands
+  
+  case class State(vouchers: Set[VoucherId]) {
+    def addVoucher(voucherId: VoucherId) = copy(vouchers = vouchers + voucherId)
+    def removeVoucher(voucherId: VoucherId) = copy(vouchers = vouchers - voucherId)
+  }
+  object State {
+    def empty: State = State(vouchers = Set.empty)
+  }
+  
+  val commandHandler: CommandHandler[Commands, State] = 
+    commands => 
+    state => 
+    commands match {
+      case AddVoucher(voucherId) if state.contains(voucherId) => Left("Cannot add voucher if already added")
+      case AddVoucher(voucherId) => Right(state.addVoucher(voucherId))
+      case RemoveVoucher(voucherId) if !state.contains(voucherId) => Left("Voucher not found")
+      case RemoveVoucher(voucherId)  => Right(state.removeVoucher(voucherId))
+    }
+  
+}
